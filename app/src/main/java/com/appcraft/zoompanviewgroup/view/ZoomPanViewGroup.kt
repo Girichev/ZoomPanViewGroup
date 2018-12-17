ae06159154d023bcb7e566f157732f71a3feb4c8
girichev <girichev@gmail.com>
2018-12-17 12:20:41 +0300

    New offsets

--- app/src/main/java/com/appcraft/zoompanviewgroup/view/ZoomPanViewGroup.kt
+++ app/src/main/java/com/appcraft/zoompanviewgroup/view/ZoomPanViewGroup.kt
@@ -2,14 +2,13 @@ package com.appcraft.zoompanviewgroup.view
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.PointF
-import android.graphics.Rect
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.SizeF
 import android.view.*
 import android.widget.RelativeLayout
 import android.widget.Scroller
+import java.lang.ref.WeakReference
 
 
 fun Float.clamped(min: Float, max: Float): Float {
@@ -28,6 +27,9 @@ fun ViewGroup.getAllChildren(v: View = this): List<View> {
 class ZoomPanViewGroup : RelativeLayout, ScaleGestureDetector.OnScaleGestureListener,
     GestureDetector.OnGestureListener {
 
+    private val clickListener: WeakReference<OnClickListener>? = null
+    private val lastClick = PointF()
+
     private val mScaleDetector = ScaleGestureDetector(context, this)
     private val mScrollDetector = GestureDetector(context, this)
     private val scroller = Scroller(context)
@@ -43,12 +45,15 @@ class ZoomPanViewGroup : RelativeLayout, ScaleGestureDetector.OnScaleGestureList
     private val layoutComputationRect = Rect()
 
     private val maxOffset: PointF
-        get() = PointF(-mLeft * scale, -mTop * scale)
+        get() = PointF(
+            Math.max(0.0f, (width - size.width - mLeft)),
+            Math.max(0.0f, (height - size.height - mTop))
+        )
 
     private val minOffset: PointF
         get() = PointF(
-            Math.min(0.0f, (width - size.width)),
-            Math.min(0.0f, (height - size.height))
+            Math.min(0.0f, (width - size.width - mLeft)),
+            Math.min(0.0f, (height - size.height - mTop))
         )
 
     private val size: SizeF
@@ -73,7 +78,13 @@ class ZoomPanViewGroup : RelativeLayout, ScaleGestureDetector.OnScaleGestureList
     override fun onShowPress(p0: MotionEvent?) {}
     override fun onLongPress(p0: MotionEvent?) {}
     override fun onDown(p0: MotionEvent?) = true
-    override fun onSingleTapUp(p0: MotionEvent?) = true
+    override fun onSingleTapUp(p0: MotionEvent): Boolean {
+        clickListener?.get()?.onClick(p0.x, p0.y)
+        lastClick.x = p0.x
+        lastClick.y = p0.y
+        redraw()
+        return true
+    }
 
     override fun onScaleEnd(p0: ScaleGestureDetector) {}
     override fun onScaleBegin(p0: ScaleGestureDetector) = true
@@ -142,6 +153,24 @@ class ZoomPanViewGroup : RelativeLayout, ScaleGestureDetector.OnScaleGestureList
         canvas.scale(scale, scale)
         super.dispatchDraw(canvas)
         canvas.restore()
+        val step = height / 60f
+
+        canvas.drawText("mLeft: $mLeft, mTop: $mTop, mRight: $mRight, mBottom: $mBottom", step, step * 2, Paint().apply {
+            textSize = step
+            color = Color.GREEN
+        })
+        canvas.drawText("MIN_X: ${minOffset.x}, MAX_X: ${maxOffset.x}, MIN_Y: ${minOffset.y} MAX_Y: ${maxOffset.y}", step, step * 4, Paint().apply {
+            textSize = step
+            color = Color.GREEN
+        })
+        canvas.drawText("X: ${offset.x}, Y: ${offset.y}", step, step * 6, Paint().apply {
+            textSize = step
+            color = Color.GREEN
+        })
+        canvas.drawText("LAST_CLICK_X: ${lastClick.x}, LAST_CLICK_Y: ${lastClick.y}", step, step * 8, Paint().apply {
+            textSize = step
+            color = Color.GREEN
+        })
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
@@ -151,12 +180,12 @@ class ZoomPanViewGroup : RelativeLayout, ScaleGestureDetector.OnScaleGestureList
         mRight = 0f
         mBottom = 0f
         getAllChildren().forEach { child ->
-            if (child.visibility != View.GONE) {
+            if (child.visibility != View.GONE && (child !is ViewGroup)) {
                 layoutComputationRect.apply {
                     child.getDrawingRect(this@apply)
                     this@ZoomPanViewGroup.offsetDescendantRectToMyCoords(child, this@apply)
-                    mLeft = Math.min(mLeft, this.left.toFloat())
-                    mTop = Math.min(mTop, this.top.toFloat())
+                    mLeft = Math.max(mLeft, this.left.toFloat())
+                    mTop = Math.max(mTop, this.top.toFloat())
                     mRight = Math.max(mRight, this.right.toFloat())
                     mBottom = Math.max(mBottom, this.bottom.toFloat())
                 }
@@ -172,5 +201,11 @@ class ZoomPanViewGroup : RelativeLayout, ScaleGestureDetector.OnScaleGestureList
         mScrollDetector.onTouchEvent(event)
         mScaleDetector.onTouchEvent(event)
         return true
+    }
+
+    interface OnClickListener {
+        fun onClick(x: Float, y: Float)
+        fun onDoubleTap(x: Float, y: Float)
+        fun onLongPress(x: Float, y: Float)
     }
 }
